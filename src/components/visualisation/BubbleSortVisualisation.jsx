57abd4f9c44d4075133bb7dbd92e8e412bac8703
Luke Dunn <lukedunn011@gmail.com>
2024-12-08 23:01:58 +1100

    added a speed dial

--- src/components/visualisation/BubbleSortVisualisation.jsx
+++ src/components/visualisation/BubbleSortVisualisation.jsx
@@ -1,14 +1,17 @@
 import { SortingVisualizer } from './SortingVisualizer';
+import { useState } from 'react';
 
 const bubbleSortAlgorithm = async (array, { setArray, setComparing, setSwapping, speed }) => {
     const arr = [...array];
     const n = arr.length;
 
+    const baseSpeed = 1000;
+    console.log("reached")
     for (let i = 0; i < n - 1; i++) {
         for (let j = 0; j < n - i - 1; j++) {
             setComparing([j, j + 1]);
-            await new Promise(resolve => setTimeout(resolve, speed));
-            // delaying each swap and compare algorithm to a set speed so it can be shown
+            await new Promise(resolve => setTimeout(resolve, baseSpeed / speed));
+            
             if (arr[j] > arr[j + 1]) {
                 setSwapping([j, j + 1]);
                 await new Promise(resolve => setTimeout(resolve, speed / 2));
@@ -24,10 +27,14 @@ const bubbleSortAlgorithm = async (array, { setArray, setComparing, setSwapping,
 };
 
 export default function BubbleSortVisualisation() {
+    const [speed, setSpeed] = useState(50);
+
     return (
         <SortingVisualizer
             title="bubble sort"
             sortingFunction={bubbleSortAlgorithm}
+            sortingSpeed={speed}
+            setSpeed={setSpeed}
         />
     );
 }
